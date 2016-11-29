ac745a13cdb0fd05fb8220e533dee32d9fc0b13d
Edmund Rhudy <erhudy@users.noreply.github.com>
2016-11-29 13:28:54 -0500

    Re-organize bootstrap attributes into a separate file

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -461,26 +461,6 @@ default['bcpc']['hardware']['powersave'] = false
 
 ###########################################
 #
-# defaults for the bcpc.bootstrap settings
-#
-###########################################
-#
-# A value of nil means to let the Ubuntu installer work it out - it
-# will try to find the nearest one. However the selected mirror is
-# often slow.
-default['bcpc']['bootstrap']['mirror'] = nil
-#
-# if you do specify a mirror, you can adjust the file path that comes
-# after the hostname in the URL here
-default['bcpc']['bootstrap']['mirror_path'] = "/ubuntu"
-#
-# worked example for the columbia mirror mentioned above which has a
-# non-standard path
-#default['bcpc']['bootstrap']['mirror']      = "mirror.cc.columbia.edu"
-#default['bcpc']['bootstrap']['mirror_path'] = "/pub/linux/ubuntu/archive"
-
-###########################################
-#
 # Rally settings
 #
 ###########################################
@@ -683,12 +663,3 @@ default['bcpc']['getty']['ttys'] = %w( ttyS0 ttyS1 )
 # VNC uses cluster domain name by default
 # for proxy base url. Set to 'true' to use vip
 default['bcpc']['vnc']['proxy_use_vip'] = false
-
-###########################################
-#
-#  Bootstrap tftpd settings
-#
-###########################################
-#
-# Address and port to listen
-default['bcpc']['tftpd']['address'] = ':69'
