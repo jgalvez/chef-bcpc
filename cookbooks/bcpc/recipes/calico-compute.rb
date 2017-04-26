87c663ef7e93cc438076c9ba6718f0f12f5b7003
Edmund Rhudy <erhudy@bloomberg.net>
2017-04-26 10:34:44 -0400

    Add neutron.conf to compute nodes via neutron-common recipe
    Neutron metadata agent needs neutron.conf to be configured in order to
    connect to AMQP.

--- cookbooks/bcpc/recipes/calico-compute.rb
+++ cookbooks/bcpc/recipes/calico-compute.rb
@@ -21,6 +21,7 @@ return unless node['bcpc']['enabled']['neutron']
 
 include_recipe 'bcpc::etcd-common'
 include_recipe 'bcpc::bird-compute'
+include_recipe 'bcpc::neutron-common'
 include_recipe 'bcpc::packages-calico'
 
 file '/etc/init/neutron-dhcp-agent.override' do
@@ -74,5 +75,6 @@ end
 %w(neutron-metadata-agent calico-dhcp-agent calico-felix).each do |svc|
   service svc do
     action [:start, :enable]
+    subscribes :restart, 'template[/etc/neutron/neutron.conf]', :delayed
   end
 end
